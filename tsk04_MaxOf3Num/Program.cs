0e7f6a1026da05129a4809e716a334cb9cd8d0a9
Shoshela <goyati@mail.ru>
2022-11-18 19:06:00 +0300

    Add tsk04_MaxOf3Num (Нахождение максимального из трех чисел)

---
+++ tsk04_MaxOf3Num/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите числа...");
+int a=Convert.ToInt32(Console.ReadLine());
+int b=Convert.ToInt32(Console.ReadLine());
+int c=Convert.ToInt32(Console.ReadLine());
+
+if ((b < a) && (a > c))
+    {
+        Console.Write("Максимальное из введенных чисел - ");
+        Console.WriteLine(a);
+    }
+
+    else if ((a < b) && (b > c))
+    {
+        Console.Write("Максимальное из введенных чисел - ");
+        Console.WriteLine(b);
+    }
+
+    else
+    {
+        Console.Write("Максимальное из введенных чисел - ");
+        Console.WriteLine(c);
+    }
