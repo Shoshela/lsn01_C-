1ed1977881da319627ff6f33caf957da8de5bf13
Shoshela <goyati@mail.ru>
2022-11-18 19:13:37 +0300

    Add tsk06_EvenOrNot (определение четности числа)

---
+++ tsk06_EvenOrNot/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число...");
+int a=Convert.ToInt32(Console.ReadLine());
+if ( a % 2 == 0)
+{
+    Console.Write("Число четное!");
+}
+else
+{
+    Console.Write("Число нечетное!");
+}
