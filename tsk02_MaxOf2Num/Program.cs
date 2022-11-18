4ab6b3485b0266f9371471793e82f1af74addadc
Shoshela <goyati@mail.ru>
2022-11-18 18:43:59 +0300

    Add tsk02_MaxOf2Num (Нахождение наибольшего из двух чисел

---
+++ tsk02_MaxOf2Num/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите первое число: ");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b=Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.Write("Большее из введенных чисел - ");
+    Console.WriteLine(a);
+    Console.Write("Меньшее из введенных чисел - ");
+    Console.WriteLine(b);
+}
+else
+{
+    Console.Write("Большее из введенных чисел - ");
+    Console.WriteLine(b);
+    Console.Write("Меньшее из введенных чисел - ");
+    Console.WriteLine(a);
+}
